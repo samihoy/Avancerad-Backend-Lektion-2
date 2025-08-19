59bfb83093e4feb6421bf7ede7f57766a7f602e6
Bror Lundberg <maximillian.lundber@chasacademy.se>
2025-08-19 23:40:17 +0200

    created the database and added some dbset's

---
+++ Data/MovieRentalDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Avancerad_Backend_Lektion_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avancerad_Backend_Lektion_2.Data
+{
+    public class MovieRentalDBContext : DbContext
+    {
+        public MovieRentalDBContext(DbContextOptions<MovieRentalDBContext> options) : base (options)
+        {
+            
+        }
+
+        DbSet<User> Users { get; set; }
+        DbSet<Movie> Movies { get; set; }
+        DbSet<UserMovies> Loan { get; set; }
+
+    }
+}
