026c8e7790df51b06d331c1b8fddc53d3a172005
Bror Lundberg <maximillian.lundber@chasacademy.se>
2025-08-19 22:17:55 +0200

    added the main database to builder and added a connection string

---
+++ Program.cs
@@ -0,0 +1,44 @@
+
+using Avancerad_Backend_Lektion_2.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avancerad_Backend_Lektion_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<MovieRentalDBContext> (options =>
+            {
+                options.UseSqlServer((builder.Configuration.GetConnectionString("DefaultConnection")));
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
